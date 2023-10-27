83dfb48017f3908622ac0166c7f75db155bd6b52
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-27 19:44:33 -0300

    add

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const element = document.querySelector('#text-desenvolvedor');
+const text = "Desenvolvedor Full-Stack";
+
+if (element != null) {
+    function trocandoTexto(elementoIndex, text) {
+        const caracters = text.split("").reverse();
+        const digitando = setInterval(() => {
+            
+            if (!caracters.length) {
+                return clearInterval(digitando);
+            }
+
+            const proxima = caracters.pop();
+            element.innerHTML += (proxima);
+        }, 120);
+    }
+    trocandoTexto(element, text);
+}
+
+const botaoTopo = document.querySelector('#btn-topo');
+
+botaoTopo.addEventListener('click', function () { 
+    window.scrollTo(0,0);
+});
+
+document.addEventListener('scroll', function () {
+    if (window.scrollY > 30) {
+        botaoTopo.style.display = 'flex';
+    } else {
+        botaoTopo.style.display = 'none';
+    }
+});
+
+const entradaPadrao = document.querySelector('.entrada-padrao');
+
+if (entradaPadrao != null) { 
+    const observadorTxt = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+           if (entry.isIntersecting) {
+               entry.target.classList.add('text-entrando');
+           } 
+        });
+    });
+    
+    const elementosTxt = document.querySelectorAll('.entrada-padrao');
+    
+    elementosTxt.forEach(elemento => observadorTxt.observe(elemento));
+}
+
+const entradaLateral = document.querySelector('.entrada-lateral');
+
+if (entradaLateral != null) {
+    const observadorTxt2 = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('text-entrando-lateral');
+            }
+        });
+    });
+
+    const elementosTxt2 = document.querySelectorAll('.entrada-lateral');
+
+    elementosTxt2.forEach(elemento => observadorTxt2.observe(elemento));
+}
