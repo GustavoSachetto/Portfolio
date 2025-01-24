985acb40e3bdfae5f26b8ca686f523f1c48410db
Gustavo Sachetto <g.sachettocruz@gmail.com>
2025-01-24 10:33:38 -0300

    feat: implementa mudanças no layout

---
+++ assets/js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 15000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
