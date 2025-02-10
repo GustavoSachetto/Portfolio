23e01fef12b2f91006f3c526c9983f54cbe14769
Gustavo Sachetto <g.sachettocruz@gmail.com>
2025-02-10 19:37:21 -0300

    perf: melhora desempenho do site na hospedagem

--- README.md
+++
@@ -1,6 +0,0 @@
-# Bem-vindo! 👋
-Obrigado por conferir meu portfolio.
-
-__link:__ https://gustavosachetto.site/
-
-__Tecnologias utilizadas:__ HTML, CSS e JS.
--- assets/js/google-analytics.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-
-gtag('js', new Date());
-gtag('config', 'G-9FGJ6QW4D8');
